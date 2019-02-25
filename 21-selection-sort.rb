e076a3bdc0651980893932375cc8ad247cafc0c9
javiel <zehua046@gmail.com>
2019-02-25 21:32:55 +0800

    21-selection-sort

--- 21-selection-sort.rb
+++ 21-selection-sort.rb
@@ -2,7 +2,34 @@
 # https://zh.wikipedia.org/wiki/选择排序
 
 def selection_sort(arr)
-  #...
+  i = 0
+  l = arr.size - 1
+  while ( i < l )
+      min = i
+      j = i + 1
+      while (j < arr.size )
+          if arr[min] > arr[j]
+              min = j
+          end
+          j += 1
+      end
+      temp = arr[min]
+      arr[min] = arr[i]
+      arr[i] = temp
+      i += 1
+  end
+  return arr
+
+  
+  # even_arr = []
+  # i = 0
+  # while [i <= (arr.size-1)]
+  #   j = arr.min
+  #   arr = arr - (j.to_s)
+  #   even_arr << j
+  #   i+=1
+  # end
+  # return even_arr
 end
 
 arr =  [7, 68, 42, 46, 9, 91, 77, 46, 86, 1]
