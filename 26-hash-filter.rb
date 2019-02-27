9f0cbba2901e65be7c740df940d9215bbb1e4b6e
javiel <zehua046@gmail.com>
2019-02-27 13:46:04 +0800

    26-hash-filter

--- 26-hash-filter.rb
+++ 26-hash-filter.rb
@@ -8,13 +8,27 @@ arr = [
   { "name" => "Vincent", "age" => 6 },
 ]
 
-# ....
+def filter_arr(arr)
+  arr1 = []
+  arr2 = []
+  arr.each do |h|
+    if h["age"] >= 18
+      arr1.push(h)
+    end
+  end
+  arr2 = arr1.sort_by do |i|
+    i["age"]
+  end
+  return arr2
 
-puts "所有成年人，并由小到大: _________"
+  # return arr1.sort_by{|i| i["age"]}.to_s
+end
+
+puts "所有成年人，并由小到大: #{filter_arr(arr)}"
 
 # 答案应该是
 #[
 #  { "name" => "Steven", "age" => 22 },
 #  { "name" => "Peter", "age" => 30 },
 #  { "name" => "David", "age" => 45 }
-#]
+# ]
