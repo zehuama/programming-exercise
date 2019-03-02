5b71357bf07fa59c359749e4bc40266528beaa6b
javiel <zehua046@gmail.com>
2019-03-03 00:33:28 +0800

    28-word-count

--- 28-word-count.rb
+++ 28-word-count.rb
@@ -2,4 +2,58 @@
 
 doc = File.read("wordcount.txt")
 
-# ...
+
+# way1
+# count = Hash.new(0)
+# doc.each_line do |line|
+#   line = line.chomp
+#   line = line.gsub(/[,'".]/, '')
+#   line = line.strip
+#   words = line.split("")
+#   words.each do |word|
+#     count[word]+=1
+#   end
+# end
+# puts "每个单字出现的次数如下："
+# count.each do |key, value|
+#   puts "#{key}:#{value}"
+# end
+
+# way2
+# count = Hash.new(0)
+# doc.each_line do |line|
+#   line = line.gsub(/[,'".]/, '')
+#   line = line.strip
+#   words = line.split("")
+#   words.each { |word| count[word] += 1 }
+# end
+#
+# puts "每个单字出现的次数如下："
+# count.each do |key, value|
+#   puts "#{key}:#{value}"
+# end
+
+# way3
+# words = doc.downcase.scan(/\w+/)
+# result = {}
+# words.each do |i|
+#   result[i] = words.count(i)
+# end
+# words.each {|i| result[i] = words.count(i)}
+
+# result = words.each_with_object(Hash.new(0)) { |i, hash| hash[i] += 1 }
+# puts result
+
+# way4
+# words = doc.downcase.scan(/\w+/)
+# words.uniq.each do |i|
+#   puts "#{i} 出现#{words.count(i)}次"
+# end
+
+# way5
+arr = doc.split("")
+h = {}
+arr.uniq.each do |i|
+  h[i] = arr.count(i)
+end
+puts h
